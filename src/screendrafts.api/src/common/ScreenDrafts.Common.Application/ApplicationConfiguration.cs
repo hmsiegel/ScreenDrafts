169318a2e83bd67925507e121194a59aa9012d6e
MondayNeurosis <49638789+hmsiegel@users.noreply.github.com>
2025-01-26 21:01:05 -0500

    Adds Validaion Pipeline Behavior

--- src/screendrafts.api/src/common/ScreenDrafts.Common.Application/ApplicationConfiguration.cs
+++ src/screendrafts.api/src/common/ScreenDrafts.Common.Application/ApplicationConfiguration.cs
@@ -9,6 +9,7 @@ public static class ApplicationConfiguration
       config.RegisterServicesFromAssemblies(moduleAssemblies);
       config.AddOpenBehavior(typeof(ExceptionHandlingPipelineBehavior<,>));
       config.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
+      config.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
     });
 
     services.AddValidatorsFromAssemblies(moduleAssemblies);
--- src/screendrafts.api/src/common/ScreenDrafts.Common.Application/GlobalUsings.cs
+++ src/screendrafts.api/src/common/ScreenDrafts.Common.Application/GlobalUsings.cs
@@ -2,6 +2,7 @@
 global using System.Reflection;
 
 global using FluentValidation;
+global using FluentValidation.Results;
 
 global using MediatR;
 
@@ -11,6 +12,7 @@ global using Microsoft.Extensions.Logging;
 global using ScreenDrafts.Common.Application.Behaviors;
 global using ScreenDrafts.Common.Application.Exceptions;
 global using ScreenDrafts.Common.Application.Logging;
+global using ScreenDrafts.Common.Application.Messaging;
 global using ScreenDrafts.Common.Domain;
 
 global using Serilog.Context;
