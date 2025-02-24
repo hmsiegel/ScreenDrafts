03aa1970a784356d3f3c7dbeb655e9c7ee8781be
MondayNeurosis <49638789+hmsiegel@users.noreply.github.com>
2025-02-24 14:42:27 -0500

    test(drafts): updated the tests to add a draft pick to the draft

--- src/screendrafts.api/src/modules/drafts/ScreenDrafts.Modules.Drafts.Application/Drafts/Commands/AddPick/AddPickCommandHandler.cs
+++ src/screendrafts.api/src/modules/drafts/ScreenDrafts.Modules.Drafts.Application/Drafts/Commands/AddPick/AddPickCommandHandler.cs
@@ -20,21 +20,32 @@ internal sealed class AddPickCommandHandler(
       return Result.Failure<Guid>(DraftErrors.NotFound(request.DraftId));
     }
 
+    if (draft.DraftStatus != DraftStatus.InProgress)
+    {
+      return Result.Failure<Guid>(DraftErrors.DraftNotStarted);
+    }
+
     var drafter = draft.Drafters.FirstOrDefault(d => d.Id.Value == request.DrafterId);
 
     if (drafter is null)
     {
-      return Result.Failure<Guid>(DrafterErrors.NotFound(drafter!.Id.Value));
+      return Result.Failure<Guid>(DrafterErrors.NotFound(request.DrafterId));
     }
 
     var movie = await _draftsRepository.GetMovieByIdAsync(request.MovieId, cancellationToken);
 
     if (movie is null)
     {
-      return Result.Failure<Guid>(DraftErrors.MovieNotFound(movie!.Id));
+      return Result.Failure<Guid>(DraftErrors.MovieNotFound(request.MovieId));
     }
 
-    draft.AddPick(request.Position, movie, drafter);
+    var result = draft.AddPick(request.Position, movie, drafter);
+
+    if (result.IsFailure)
+    {
+      return Result.Failure(result.Errors);
+    }
+
     _picksRepository.Add(draft.Picks.Last());
     await _unitOfWork.SaveChangesAsync(cancellationToken);
 
