e5bb3a3bb70e2cdf0ace8acfb81e577920fb8dcc
MondayNeurosis <49638789+hmsiegel@users.noreply.github.com>
2025-02-24 13:14:27 -0500

    fix(drafts): updated the getdraftqueryhandler to use efcore instead of dapper

--- src/screendrafts.api/src/modules/drafts/ScreenDrafts.Modules.Drafts.Application/Drafts/Queries/GetDraft/DraftResponse.cs
+++ src/screendrafts.api/src/modules/drafts/ScreenDrafts.Modules.Drafts.Application/Drafts/Queries/GetDraft/DraftResponse.cs
@@ -1,24 +1,36 @@
-﻿namespace ScreenDrafts.Modules.Drafts.Application.Drafts.Queries.GetDraft;
+﻿using System.Collections.ObjectModel;
+
+namespace ScreenDrafts.Modules.Drafts.Application.Drafts.Queries.GetDraft;
 
 public sealed record DraftResponse(
     Guid Id,
     string Title,
-    string DraftType,
+    int DraftType,
     int TotalPicks,
     int TotalDrafters,
     int TotalHosts,
-    string DraftStatus)
+    int EpisodeType,
+    int DraftStatus)
 {
+  private readonly List<DrafterResponse> _drafters = [];
+  private readonly List<HostResponse> _hosts = [];
+
   public DraftResponse()
     : this(
         Guid.Empty,
         string.Empty,
-        string.Empty,
         default,
         default,
         default,
-        string.Empty)
+        default,
+        default,
+        default)
   {
-
   }
+
+  public ReadOnlyCollection<DrafterResponse> Drafters => _drafters.AsReadOnly();
+  public ReadOnlyCollection<HostResponse> Hosts => _hosts.AsReadOnly();
+
+  public void AddDrafter(DrafterResponse drafter) => _drafters.Add(drafter);
+  public void AddHost(HostResponse host) => _hosts.Add(host);
 }
