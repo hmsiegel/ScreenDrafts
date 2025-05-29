e1b344062e425bd8dd28da155077a6ce4460c264
MondayNeurosis <49638789+hmsiegel@users.noreply.github.com>
2025-05-28 21:12:11 -0400

    feat(drafts): add delete drafts
    Add DeleteDraft functionality with command and handler
    
    This commit introduces the `DeleteDraftCommand` to encapsulate the data needed for deleting a draft identified by a `DraftId`. A corresponding handler, `DeleteDraftCommandHandler`, processes the command, checks for the draft's existence, and deletes it if found.
    
    Additionally, a validator ensures the `DraftId` is valid, and a new HTTP DELETE endpoint is established in `DeleteDraft.cs` to handle draft deletion requests. A request model, `DeleteDraftRequest`, captures the draft ID from the route, and a summary class documents the expected responses for the endpoint.

---
+++ src/screendrafts.api/src/modules/drafts/ScreenDrafts.Modules.Drafts.Application/Drafts/Commands/DeleteDraft/DeleteDraftCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace ScreenDrafts.Modules.Drafts.Application.Drafts.Commands.DeleteDraft;
+
+internal sealed class DeleteDraftCommandValidator : AbstractValidator<DeleteDraftCommand>
+{
+  public DeleteDraftCommandValidator()
+  {
+    RuleFor(x => x.DraftId)
+        .NotEmpty().WithMessage("Draft ID must not be empty.")
+        .Must(id => id.BeValidGuid()).WithMessage("Draft ID should be a valid GUID.");
+  }
+}
