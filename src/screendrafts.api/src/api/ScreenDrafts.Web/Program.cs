8842bae7982fa26c19aec2e8fecbc0d405392291
MondayNeurosis <49638789+hmsiegel@users.noreply.github.com>
2025-06-27 08:27:49 -0400

    fix(docker): update configurations
    Remove keycloak-db service and update configurations
    
    - Removed `keycloak-db` service from `compose-config.yml`.
    - Updated `screendrafts.database` and `screendrafts.identity` services with necessary environment variables.
    - Modified `screendrafts.web` service in `docker-compose.yml` to set `ASPNETCORE_ENVIRONMENT` to "Development" and changed volume mapping to a relative path.
    - Updated `launchSettings.json` to include multiple services for starting without debugging.
    - Adjusted environment variables for Docker container in `launchSettings.json`.
    - Added Swagger support in `Program.cs` for improved API documentation and testing.

--- src/screendrafts.api/src/api/ScreenDrafts.Web/Program.cs
+++ src/screendrafts.api/src/api/ScreenDrafts.Web/Program.cs
@@ -69,6 +69,7 @@ app.UseFastEndpoints(c =>
 if (app.Environment.IsDevelopment())
 {
   app.UseOpenApi(c => c.Path = "/openapi/{documentname}.json");
+  app.UseSwaggerGen();
   app.UseSwaggerUi();
   app.MapOpenApi();
   app.MapScalarApiReference();
