d936214fd471f99082387d5c98b841be45942b57
MondayNeurosis <49638789+hmsiegel@users.noreply.github.com>
2025-08-25 21:02:58 -0400

    test(common): Add integration testing framework
    Introduce `BaseIntegrationTest<TDbContext>` for structured integration tests with dependency injection and database management. Add `IntegrationTestWebAppFactory` to manage test containers and environment setup. Implement `TestFakerProvider` for test data generation. Create `GlobalUsings.cs` for common namespaces. Update project file with necessary package references for testing.

---
+++ src/screendrafts.api/src/common/ScreenDrafts.Common.IntegrationTests/Abstractions/BaseIntegrationTest.cs
@@ -0,0 +1,83 @@
+﻿namespace ScreenDrafts.Common.IntegrationTests.Abstractions;
+
+public abstract partial class BaseIntegrationTest<TDbContext> : IDisposable, IAsyncLifetime
+  where TDbContext : DbContext
+{
+  private bool _disposedValue;
+  protected static Faker Faker => TestFakerProvider.Faker;
+
+  protected IServiceScope ServiceScope { get; }
+  protected ISender Sender { get; }
+  protected HttpClient HttpClient { get; }
+  protected TDbContext DbContext { get; }
+  protected IntegrationTestWebAppFactory Factory { get; }
+
+  protected BaseIntegrationTest(IntegrationTestWebAppFactory factory)
+  {
+    ArgumentNullException.ThrowIfNull(factory);
+
+    Factory = factory;
+    ServiceScope = factory.Services.CreateScope();
+    Sender = ServiceScope.ServiceProvider.GetRequiredService<ISender>();
+    HttpClient = factory.CreateClient();
+    DbContext = ServiceScope.ServiceProvider.GetRequiredService<TDbContext>();
+  }
+
+  protected abstract Task ClearDatabaseAsync();
+
+  protected T GetService<T>()
+      where T : notnull
+  {
+    return ServiceScope.ServiceProvider.GetRequiredService<T>();
+  }
+
+  protected T? GetOptionalService<T>()
+      where T : class
+  {
+    return ServiceScope.ServiceProvider.GetService<T>();
+  }
+
+  protected virtual void Dispose(bool disposing)
+  {
+    if (!_disposedValue)
+    {
+      if (disposing)
+      {
+        ServiceScope.Dispose();
+        HttpClient.Dispose();
+        DbContext.Dispose();
+      }
+
+      _disposedValue = true;
+    }
+  }
+
+  public void Dispose()
+  {
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
+
+  public async Task DisposeAsync()
+  {
+    await ClearDatabaseAsync();
+    await OnDisposeAsync();
+    Dispose();
+  }
+
+  protected virtual Task OnDisposeAsync()
+  {
+    return Task.CompletedTask;
+  }
+
+  public async Task InitializeAsync()
+  {
+    await ClearDatabaseAsync();
+    await OnInitializeAsync();
+  }
+
+  protected virtual Task OnInitializeAsync()
+  {
+    return Task.CompletedTask;
+  }
+}
