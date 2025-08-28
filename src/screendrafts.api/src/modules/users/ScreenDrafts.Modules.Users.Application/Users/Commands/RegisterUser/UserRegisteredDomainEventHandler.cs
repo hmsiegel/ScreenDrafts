873b8403a50824609883a09c2dc865617f93e0ac
MondayNeurosis <49638789+hmsiegel@users.noreply.github.com>
2025-08-28 16:07:11 -0400

    feat(users): adds UserRegisteredDomainEventHandler
    Add UserRegisteredDomainEventHandler class
    
    Introduces the `UserRegisteredDomainEventHandler` class in the `ScreenDrafts.Modules.Users.Application.Users.Commands.RegisterUser` namespace. This class handles `UserRegisteredDomainEvent` by sending a `GetUserQuery` and publishing a `UserRegisteredIntegrationEvent` upon successful retrieval of user details. Dependencies on `ISender` and `IEventBus` are injected via the constructor.

---
+++ src/screendrafts.api/src/modules/users/ScreenDrafts.Modules.Users.Application/Users/Commands/RegisterUser/UserRegisteredDomainEventHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace ScreenDrafts.Modules.Users.Application.Users.Commands.RegisterUser;
+
+internal sealed class UserRegisteredDomainEventHandler(ISender sender, IEventBus eventBus)
+  : DomainEventHandler<UserRegisteredDomainEvent>
+{
+  private readonly ISender _sender = sender;
+  private readonly IEventBus _eventBus = eventBus;
+
+  public override async Task Handle(
+    UserRegisteredDomainEvent domainEvent,
+    CancellationToken cancellationToken = default)
+  {
+    var result = await _sender.Send(
+      new GetUserQuery(domainEvent.UserId),
+      cancellationToken);
+
+    if (result.IsFailure)
+    {
+      throw new ScreenDraftsException(nameof(GetUserQuery), result.Error);
+    }
+
+    await _eventBus.PublishAsync(
+      new UserRegisteredIntegrationEvent(
+        domainEvent.Id,
+        domainEvent.OccurredOnUtc,
+        result.Value.UserId,
+        result.Value.Email,
+        result.Value.FirstName,
+        result.Value.LastName,
+        result.Value.MiddleName),
+      cancellationToken);
+  }
+}
